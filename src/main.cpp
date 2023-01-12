6ee6068668e6967a67c981dc527d8542a1b1aecc
Rizky Widianto <75198644+rizkywidianto12@users.noreply.github.com>
2023-01-12 09:36:22 +0700

    UPDATE

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 
 /*
 KALIBRASI SENSOR PRESSURE
-PRESSURE SENSOR OUTPUT
-*/
 
-// ISI DISINI 
+
+- KONVERSI KE BAR > LIHAT BARIS 134 - 135
+*/
 const int BAR_0 = 0 ;
 const int BAR_1 = 0 ;
 const int BAR_2 = 0 ;
@@ -15,6 +15,30 @@ const int BAR_4 = 0 ;
 const int BAR_5 = 0 ;
 const int BAR_6 = 0 ;
 const int BAR_7 = 0 ;
+
+
+/*
+KALIBRASI TOMBOL 
+
+~ LOGIC
+
+- BATAS ATAS
+    PRESSURE OUT > SETTING BUTTON + BATAS ATAS = VALVE BUANG NYALA
+- BATAS BAWAH 
+    RESSURE OUT < SETTING BUTTON + BATAS BAWAH = VALVE MASUK NYALA
+BACA BARIS 180
+*/
+// CHARGING VALVE / BATAS ATAS
+const int CALH19 = 1;
+const int CALH22 = 1;
+const int CALH25 = 1;
+
+// VENTING VALVE / BATAS BAWAH
+const int CALL19 = 0;
+const int CALL22 = 0;
+const int CALL25 = 0;
+
+
 
 // Konfigurasi Pin
 
@@ -28,7 +52,7 @@ const int BAR_7 = 0 ;
 #define pressure_3    4       // 2.5 BAR
 #define pressure_4    5       // RELEASE 0 BAR
 
-// solenoid valve
+// Pin solenoid valve
 #define charging_valve  11
 #define venting_valve   12
 
@@ -39,23 +63,25 @@ const int BAR_7 = 0 ;
 LiquidCrystal_I2C lcd(i2c_addr,col,row);  //LCD address
 
 // variable
-int aktif;     // mode tombol
-float presIn; // pressure sensor input
-float prsOut; // pressure sensor output
-float preset; // spressure set
-float raw_1;
+int aktif;      // mode tombol
+float presIn;   // pressure sensor input
+float prsOut;   // pressure sensor output
+float preset;   // spressure set
+float raw_1;    // Nilai sensor 1( belum di konvert ke bar)
 float raw_2;
+float CALH = 0, CALL = 0;
 
 // debug
 bool V1_STATE;
 bool V2_STATE;
 bool update ; // update parameter
-bool comport;
-int counting;
+int counting; // delay update lcd
 
 void report(){
   /*
-  #       B C U   P R O T O
+  Layout LCD
+  # # # # # # # # # # # # # # # #
+  # B C U   P R O T O
   # I N   = # # # # # # # # B A R
   # O U T = # # # # # # # # B A R
   # S E T = # # # # # # # # B A R
@@ -80,7 +106,6 @@ float ultimap(float val,float min1, float max1, float min2, float max2){
 
 void setup() {
   Serial.begin(9600);
-  comport =  Serial.available();
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0,0);
@@ -112,37 +137,33 @@ void loop() {
   // delay(1);
   raw_1 = analogRead(pressure_sens_1);
   raw_2 = analogRead(pressure_sens_2);
-  // Kalibrasi
-  /*
-  
-  
-  presIn = ultimap(raw_1,0bar ,7bar ,0,7);
-  1 bar
-  2..
 
-  7 bar
-
-  */
   presIn = ultimap(raw_1,BAR_0,BAR_7,0,7);
   prsOut = ultimap(raw_2,BAR_0,BAR_7,0,7);
-  // put your main code here, to run repeatedly:
+  
   if(!digitalRead(pressure_1) && aktif !=1 ){
-    Serial.println("press 1");
+    // Serial.println("press 1");
     aktif = 1;
     preset = 1.9;
+    CALH = CALH19;
+    CALL = CALL19;
   }
   else if (!digitalRead(pressure_2) && aktif !=2 ){
-    Serial.println("press 2");
+    // Serial.println("press 2");
     aktif = 2;
     preset = 2.2;
+    CALH = CALH22;
+    CALL = CALL22;
   }
   else if (!digitalRead(pressure_3) && aktif !=3 ){
-    Serial.println("press 3");
+    // Serial.println("press 3");
     aktif = 3;
     preset = 2.5;
+    CALH = CALH25;
+    CALL = CALL25;
   }
   else if (!digitalRead(pressure_4)){
-    Serial.println("press 4");
+    // Serial.println("press 4");
     aktif = 4;
     preset = 0;
     while(!digitalRead(pressure_4)){
@@ -150,23 +171,33 @@ void loop() {
     }
     digitalWrite(venting_valve,0);
   }
-    // Serial.println("p");
 
-    if (prsOut < preset - 0.3 ){
-      digitalWrite(charging_valve,1);
-      digitalWrite(venting_valve,0);
-      V1_STATE = 1;
-      V2_STATE = 0;
+
+  /*
+  INGFO
+   - PRSOUT = PRESSURE SENSOR OUTPUT, SATUAN BAR
+   - PRESET = PRESSURE SETTING BUTTON
+   - CALL & CALH = SETTING BATAS BAWAH DAN BATAS ATAS
+
+  LOGIC BUKAAN RELAY
+  */
+    if (prsOut < preset + CALL ){// Jika nilai sensor out lebih kecil dari nilai yang di set
+      digitalWrite(charging_valve,1); // relay solenoid charging aktif, angin masuk
+      digitalWrite(venting_valve,0);  // relay solenoid buang mati
+      // V1_STATE = 1;
+      // V2_STATE = 0;
     }
-    else if (prsOut > preset + 0.8){
-      digitalWrite(charging_valve,0);
-      digitalWrite(venting_valve,1);
-      V1_STATE = 0;
-      V2_STATE = 1;
+
+    else if (prsOut > preset + CALH){ // Jika nilai sensor out lebih besar dari nilai yang di set
+      digitalWrite(charging_valve,0); // relay solenoid charging mati
+      digitalWrite(venting_valve,1);  // relay solenoid buang aktif, angin dibuang
+      // V1_STATE = 0;
+      // V2_STATE = 1;
     }
+
     else {
-      V1_STATE = 0;
-      V2_STATE = 0;
+      // V1_STATE = 0;
+      // V2_STATE = 0;
       digitalWrite(charging_valve,0);
       digitalWrite(venting_valve,0);
     }
